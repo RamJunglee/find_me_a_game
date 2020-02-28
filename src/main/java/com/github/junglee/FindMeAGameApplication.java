7a53802a8c760375d14607c982e317f1b5209525
Rambabu <rambabuprg16@gmail.com>
2020-02-28 20:15:28 +0530

    First commit

---
+++ src/main/java/com/github/junglee/FindMeAGameApplication.java
@@ -0,0 +1,13 @@
+package com.github.junglee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeAGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMeAGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/junglee/FindMeAGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.junglee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMeAGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
