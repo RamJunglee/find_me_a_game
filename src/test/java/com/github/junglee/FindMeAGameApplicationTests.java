2c2348bbdb174cd0e72c9d73d4e96f8d8f8f9fd1
souvikmaji <souvikmaji94@gmail.com>
2020-02-29 11:31:01 +0530

    commented out test cases

--- src/test/java/com/github/junglee/FindMeAGameApplicationTests.java
+++ src/test/java/com/github/junglee/FindMeAGameApplicationTests.java
@@ -1,13 +1,10 @@
 package com.github.junglee;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FindMeAGameApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class FindMeAGameApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
